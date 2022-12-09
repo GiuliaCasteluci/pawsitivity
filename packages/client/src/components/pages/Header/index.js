93a4f0c3efc7555ad7aa16c17272749322817338
GiuliaCasteluci <giulia.casteluci@mykenzie.snhu.edu>
2022-12-09 19:35:30 -0300

    pages, hooks folder, and context folder created

--- packages/client/src/components/pages/Header/index.js
+++ packages/client/src/components/pages/Header/index.js
@@ -6,12 +6,7 @@ import styled from 'styled-components';
 // components are written with upper case letters
 //HeaderContainer involves  the entire heading 
 const HeaderContainer = styled.header`
-        background-color: #FFF;
-        display: flex;
-        gap: 800px;
-        flex-direction: row;
-        overflow: hidden;
-        padding: 20px 10px;
+
 
 `
 
