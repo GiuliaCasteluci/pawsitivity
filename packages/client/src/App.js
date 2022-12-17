86d1cacc6fad223754cd2d3586c8ad7288f92862
DeAndre Louis II <104401371+DreLouis2@users.noreply.github.com>
2022-12-17 13:41:08 -0500

    DresChanges

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import Register from "./components/pages/Register";
 import Login from "./components/pages/Login"
 import useAuth from "./hooks/useAuth";
+import SwipePage from './components/pages/Swipe';
 
 
 //background of the page
@@ -30,6 +31,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
           <Route path="*" element={<Home />} /> 
+          <Route path="/adopt" element={<SwipePage />} />
           the user will be redirected to the login page  with path="*"  
         </Routes>
     </AppContainer>
