e4cacc8ad83ad18455c256846a746b2cf5f7f6b1
DeAndre Louis II <104401371+DreLouis2@users.noreply.github.com>
2023-01-05 20:57:33 -0500

    addingJuneBackEnd

--- packages/server/src/routes/index.js
+++ packages/server/src/routes/index.js
@@ -1,15 +1,18 @@
-import { Router } from 'express';
-import express from 'express'
+import express from 'express';
+import petFormBackEnd from './petFormBackEnd';
+import fileRoutes from './upload';
+import fileUploader from "express-fileupload"
 
-const router = express.Router()
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).send('api endpoint');
+});
 
 // All routes start with the API_URL (default '/api')
 
-router.get('/', (req, res, next) => {
-  res.status(200).send('api endpoint')
-})
-
-router.use('/login', userRouter)
+router.use('/pets', petFormBackEnd);
+router.use("/files", fileUploader, fileRoutes)
 
 
-export default router;
+module.exports = router;
