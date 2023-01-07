f9e50443bf113e6fc295b68284f03d40e8b3781b
DeAndre Louis II <104401371+DreLouis2@users.noreply.github.com>
2023-01-07 16:41:10 -0500

    UPD EmergAndPetProf

--- packages/client/src/components/pages/PetProfile/petProfile.js
+++ packages/client/src/components/pages/PetProfile/petProfile.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import styled from "styled-components";
 
-const PetProfile = ({ pet }) => {
+const PetProfile = () => {
+  const [pet, setPet] = useState({});
+  const { petId } = useParams();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/api/pets/${petId}`).then((response) => {
+      setPet(response.data);
+    });
+  }, []);
+
+
   return (
-    <div>
-      <img src={pet.image} alt={pet.name} />
-      <h1>{pet.name}</h1>
-      <p>{pet.age}</p>
-      <p>{pet.gender}</p>
-      <p>{pet.description}</p>
+
+    <div className="pet-profile">
+      <h1>Pet Profile</h1>
+      <h2>{pet.name}</h2>
+      <h2>{pet.petType}</h2>
+      <h2>{pet.age}</h2>
+      <h2>{pet.description}</h2>
+      <h2>
+        {pet ? (
+          <img className="pet-profile-img" src={`http://localhost:3001/${pet.image}`} alt="pet" />
+        ) : null}
+      </h2>
     </div>
+
   );
 };
 
