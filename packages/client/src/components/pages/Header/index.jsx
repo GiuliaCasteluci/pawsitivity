2de1a211e4c10897339e30adc2527675c1fcf54d
DeAndre Louis II <104401371+DreLouis2@users.noreply.github.com>
2023-01-10 17:14:28 -0500

    updating

--- packages/client/src/components/pages/Header/index.jsx
+++ packages/client/src/components/pages/Header/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import {NavDropdown} from 'react-bootstrap' 
 import { useProvideAuth } from "../../../hooks/useAuth";
 
 const HeaderContainer = styled.header`
@@ -51,12 +52,22 @@ function Header() {
               <Nav.Link as={Link} to="/login">
                 Sign In
               </Nav.Link>
-              <Nav.Link as={Link} to="/petForm">
-                Emergency Surrender
+              <NavDropdown title="Adoption" id="navbarScrollingDropdown">
+              <Nav.Link className="text-center" as={Link} to="/petForm">
+                Surrender
               </Nav.Link>
-              <Nav.Link as={Link} to="/emergency">
-                Available Pets
-              </Nav.Link>
+              <NavDropdown.Item className="text-center" href="/adopt">
+                Pet Tinder
+              </NavDropdown.Item>
+              <Nav.Link className="text-center" as={Link} to="/emergency">Available Pets</Nav.Link>
+              <NavDropdown.Item className="text-center" href="/likes">
+                Liked Pets
+              </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Resources" id="navbarScrollingDropdown">
+              <NavDropdown.Item className="text-center" href="/">Pet Assistance</NavDropdown.Item>
+              <NavDropdown.Item className="text-center" href="/aboutUs">About Us</NavDropdown.Item>
+          </NavDropdown>
             </Nav>
             <Nav.Link variant="outline-info" onClick={() => signout()}>
               Sign Out
